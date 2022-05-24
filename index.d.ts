60c519ef2ca1ee8359993a2a5089b3e722869c62
Posandu                    Mapa <posandumapa@gmail.com>
2022-05-24 09:33:33 +0530

    Add Typescript Declaraion?

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+interface colors {
+    Reset: any
+    Bright: any
+    Dim: any
+    Underscore: any
+    Blink: any
+    Reverse: any
+    Hidden: any
+
+    Black: any
+    Red: any
+    Green: any
+    Yellow: any
+    Blue: any
+    Magenta: any
+    Cyan: any
+    White: any
+
+    BgBlack: any
+    BgRed: any
+    BgGreen: any
+    BgYellow: any
+    BgBlue: any
+    BgMagenta: any
+    BgCyan: any
+    BgWhite: any
+}
+
+declare const c: colors
+export const c: colors
